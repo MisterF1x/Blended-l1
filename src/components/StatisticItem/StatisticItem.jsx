4d1f8fdc73c8097329e093eedb8a7a392631aa03
Roman Usenko <luckyjuse@gmail.com>
2023-04-23 12:00:52 +0300

    task 2 added

--- src/components/StatisticItem/StatisticItem.jsx
+++ src/components/StatisticItem/StatisticItem.jsx
@@ -3,7 +3,21 @@ import {
   StatisticText,
   StatisticCounter,
 } from './StatisticItem.styled';
+import { IconContext } from 'react-icons';
 
-export const StatisticItem = () => {
-  return <div>StatisticBox</div>;
+import PropTypes from 'prop-types';
+
+export const StatisticItem = ({ title, total, icon }) => {
+  return (
+    <StatisticBox>
+      <IconContext.Provider value={{ size: 30 }}>{icon}</IconContext.Provider>
+      <StatisticCounter>{total}</StatisticCounter>
+      <StatisticText>{title}</StatisticText>
+    </StatisticBox>
+  );
+};
+
+StatisticItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  total: PropTypes.number.isRequired,
 };
