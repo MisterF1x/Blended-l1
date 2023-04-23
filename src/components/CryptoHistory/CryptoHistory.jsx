8c00875193daa2d683e52e7dc34dd26f4758ace1
Maksym Myhulia <113797631+MaksymMyhulia@users.noreply.github.com>
2023-04-23 13:01:49 +0300

    add_task-4

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,43 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
+import { formatDate } from 'helpers/formatDate';
+import PropTypes from 'prop-types';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+const tableHeaders = ['№', 'price', 'amount', 'date'];
+
+
+
+export const CryptoHistory = ({ items }) => {
+  return <BaseTable>
+      <THead>
+        <tr>
+          {tableHeaders.map((item, index) => (
+            <Th key={index}>{item}</Th>
+          ))}
+        </tr>
+      </THead>
+
+  <tbody>
+    {
+      items.map(( {id, price, amount, date }, idx)=>(
+        <Tr key={id}>
+        <Td>{ idx + 1 }</Td>
+        <Td>{ price }</Td>
+        <Td>{ amount }</Td>
+        <Td>{ formatDate(date) }</Td>
+      </Tr>
+      ))
+    }
+ 
+  </tbody>
+</BaseTable>
+};
+
+CryptoHistory.protoTypes = {
+items: PropTypes.arrayOf(PropTypes.shape({
+  id: PropTypes.number.isRequired,
+  price: PropTypes.number.isRequired,
+  amount: PropTypes.number.isRequired,
+  date: PropTypes.string.isRequired,
+})
+).isRequired
 };
--- src/helpers/formateDateToNow.js
+++ src/helpers/formateDateToNow.js
@@ -3,3 +3,5 @@ import { formatDistanceToNow } from 'date-fns';
 export const formateDateToNow = date => {
   return formatDistanceToNow(new Date(date), { addSuffix: true });
 };
+
+
